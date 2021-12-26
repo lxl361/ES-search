20a1ce4ee38b7951aa7e3bad1b7e9a075f7e6ead
luxinlong <xinlong.lu@zd-automotive.cn>
2021-12-26 14:23:46 +0800

    first

---
+++ src/main/java/com/zd/esearch/EsearchApplication.java
@@ -0,0 +1,13 @@
+package com.zd.esearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zd/esearch/EsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zd.esearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
